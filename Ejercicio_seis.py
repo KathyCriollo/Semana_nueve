d36d0ef2e1be4b7b8fd7a655a980c8fadd7c147c
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-16 08:53:20 -0500

    Ejericicio seis
    Programa Terminado

---
+++ Ejercicio_seis.py
@@ -0,0 +1,9 @@
+#Ejercicio seis 
+import turtle 
+
+#numero = int(input("Introduce el numero de lados:"))
+t= turtle.Pen()
+for x in range (1,6):
+	t.forward(180-(72+72))
+	t.left(72)
+turtle.getscreen()._root.mainloop()
