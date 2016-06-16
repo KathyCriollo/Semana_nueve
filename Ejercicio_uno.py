5a794030c11bcbd53dd62a1f5230065b04f0685f
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-16 07:50:24 -0500

    Ejercicio uno
    Programa terminado

---
+++ Ejercicio_uno.py
@@ -0,0 +1,8 @@
+#Ejercicio numero 1
+
+import time
+print (time.localtime())
+time.struct_time(tm_year=2020, tm_mon=2, tm_day=23, 
+	tm_hour=22, tm_min=18, tim_sec=39,tm_wday=0, tm_yday=73, tm_isdst=0)
+
+t = time.localtime()
