33745bc235d45e4dda3c4133dcecfb5d6f6743ca
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-16 09:56:52 -0500

    Ejercicio dies
    Programa terminado

---
+++ Ejercicio_dies.py
@@ -0,0 +1,12 @@
+import time
+form tkinter import*
+tk=Tk()
+canvas= Canvas(tk,width=400, height=200)
+canvas.pack()
+canvas.create_polygon(10, 10, 10, 60, 50, 35)
+
+for x in range(0,60):
+	canvas.move(1, 5, 0)
+	tk.update()
+	time.sleep(0.05)
+tk.mainñoop()
