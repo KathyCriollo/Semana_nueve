b18bb1d306965afd9f84dd91a4bf8d4350a83ecd
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-16 08:53:05 -0500

    Ejercicio cinco
    Programa terminado

---
+++ Ejercicio_cinco.py
@@ -0,0 +1,12 @@
+#Ejercicio cinco 
+
+import turtle 
+import time
+#numero = int(input("Introduce el numero de lados:"))
+#print ("El numero que ingreso es:\n",numero)
+t= turtle.Pen()
+
+for x in range (1,9):
+	t.forward(100)
+	t.left(225)
+	time.sleep(1)
