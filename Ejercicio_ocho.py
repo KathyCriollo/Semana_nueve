3f6cc33720b75154da6655a2f5a661b300016512
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-06-16 09:52:54 -0500

    Ejercicios
    Programas terminados

---
+++ Ejercicio_ocho.py
@@ -0,0 +1,12 @@
+import turtle
+import os
+
+win=turtle.Screen()
+win.bgcolor("green")
+t = turtle.Pen()
+t.reset()
+t.color(3,0,0)
+t.begin_fill()
+t.forward(100)
+t.left(90)
+turtle.getscreen()._root.mainloop()
---
+++ Ejercicio_siete.py
@@ -0,0 +1,11 @@
+#Ejericicio siete
+import turtle
+t= turtle.Pen()
+
+def micuadrado(size):
+
+	for x in range(1, 5):
+		t.forward(size)
+		t.left(90)
+turtle.getscreen()._root.mainloop()
+numero = int(input("Introduce el numero de lados:"))
